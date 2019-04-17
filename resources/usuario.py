4352cd774c2fe1ed9c654da130ce43e580c1bf9d
Danilo Moreira <danilo.moreira@fxdata.com.br>
2019-04-17 07:49:29 -0300

    Cadastro de Usuarios Get e Delete ok

--- resources/usuario.py
+++ resources/usuario.py
@@ -1,8 +1,8 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from models.usuario import UserModel
 
 class User(Resource):
-
+    # /usuarios/{user_id}
     def get(self, user_id):
         user = UserModel.find_user(user_id)
         if user:
@@ -15,3 +15,18 @@ class User(Resource):
             user.delete_user()
             return {'message': 'User deleted.'}
         return {'message': 'User not found.'}, 404
+
+class UserRegister(Resource):
+    # /cadastro
+    def post(self):
+        atributos = reqparse.RequestParser()
+        atributos.add_argument('login', type=str, required=True, help="The field 'login' cannot be left blank.")
+        atributos.add_argument('senha', type=str, required=True, help="The field 'senha' cannot be left blank.")
+        dados = atributos.parse_args()
+
+        if UserModel.find_by_login(dados['login']):
+            return {"message": "The login '{}' already exists.".format(dados['login'])}
+
+        user = UserModel(**dados)
+        user.save_user()
+        return {'message': 'User created successfully!'}, 201 # Created
