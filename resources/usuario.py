f154b256e0a8a225ba3b5eaa06af18d4ff68edda
Danilo Moreira <danilo.moreira@fxdata.com.br>
2019-04-17 14:35:23 -0300

    Implementando Login de Usuario

--- resources/usuario.py
+++ resources/usuario.py
@@ -1,5 +1,11 @@
 from flask_restful import Resource, reqparse
 from models.usuario import UserModel
+from flask_jwt_extended import create_access_token, jwt_required
+from werkzeug.security import safe_str_cmp
+
+atributos = reqparse.RequestParser()
+atributos.add_argument('login', type=str, required=True, help="The field 'login' cannot be left blank.")
+atributos.add_argument('senha', type=str, required=True, help="The field 'senha' cannot be left blank.")
 
 class User(Resource):
     # /usuarios/{user_id}
@@ -9,6 +15,7 @@ class User(Resource):
             return user.json()
         return {'message': 'User not found.'}, 404
 
+    @jwt_required
     def delete(self, user_id):
         user = UserModel.find_user(user_id)
         if user:
@@ -19,9 +26,6 @@ class User(Resource):
 class UserRegister(Resource):
     # /cadastro
     def post(self):
-        atributos = reqparse.RequestParser()
-        atributos.add_argument('login', type=str, required=True, help="The field 'login' cannot be left blank.")
-        atributos.add_argument('senha', type=str, required=True, help="The field 'senha' cannot be left blank.")
         dados = atributos.parse_args()
 
         if UserModel.find_by_login(dados['login']):
@@ -30,3 +34,16 @@ class UserRegister(Resource):
         user = UserModel(**dados)
         user.save_user()
         return {'message': 'User created successfully!'}, 201 # Created
+
+class UserLogin(Resource):
+
+    @classmethod
+    def post(cls):
+        dados = atributos.parse_args()
+
+        user = UserModel.find_by_login(dados['login'])
+
+        if user and safe_str_cmp(user.senha, dados['senha']):
+            token_de_acesso = create_access_token(identity=user.user_id)
+            return {'access_token': token_de_acesso}, 200
+        return {'message': 'The username or password is incorrect.'}, 401 # Unauthorized
